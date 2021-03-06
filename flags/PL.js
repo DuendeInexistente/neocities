12b617095d967fa9f672526aad19de819640084a
Dude for realsies <DuendeInexistente@gmail.com>
2021-03-06 16:08:41 -0300

    WHOOPESIE DAISY

---
+++ flags/PL.js
@@ -0,0 +1,7 @@
+document.write('\
+<div class="frame smallframe bg">\
+<img class="pixels" src="16-16-ponies/PL/16-16-potion-nova.gif"><br><img class="pixels scale400" src="16-16-ponies/PL/16-16-potion-nova.gif">\
+<br> Potion Nova\
+</div>\
+<br> Total count: 1\
+');
---
+++ flags/grandtotal.js
@@ -0,0 +1,3 @@
+document.write('\
+<b>Total flags:</b><div class="titleframe">243 </div>\
+');
