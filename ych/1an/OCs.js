1eed8159903a412196c47c9f20e8f2b0cf6c7e8f
DuendeInexistente <duendeinexistente@gmail.com>
2021-11-22 02:26:34 -0300

    Added duskinito

--- ych/1an/OCs.js
+++ ych/1an/OCs.js
@@ -1,5 +1,13 @@
 document.write('\
 <div class="frame smallframe bg">\
+<img class="pixels max100" src="16-16-ponies/OCs/duskinito2.gif"><br><img class="pixels scale400" src="16-16-ponies/OCs/duskinito2.gif">\
+<br> Duskinito2\
+</div>\
+<div class="frame smallframe bg">\
+<img class="pixels max100" src="16-16-ponies/OCs/duskinito.gif"><br><img class="pixels scale400" src="16-16-ponies/OCs/duskinito.gif">\
+<br> Duskinito\
+</div>\
+<div class="frame smallframe bg">\
 <img class="pixels max100" src="16-16-ponies/OCs/garta.gif"><br><img class="pixels scale400" src="16-16-ponies/OCs/garta.gif">\
 <br> Garta\
 </div>\
