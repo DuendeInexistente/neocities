19e8b64d192bd917356f62018d8a91245a5835d2
DuendeInexistente <duendeinexistente@gmail.com>
2024-09-30 23:45:12 -0300

    gitignore

--- public/flags/Slicing.nope/ase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-for file in *.ase;do
-aseprite --batch --verbose --oneframe $file --save-as {slice}.png
-done
-
-mogrify -background none -gravity center -extent 16x16  $file
-rm $file
-done
--- public/santa2022/upd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm things.md
-for file in *.png; do
-        echo \!\[\]\($file\)>>things.md
-done
